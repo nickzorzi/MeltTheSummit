e301ab977c4074e6c5672380ad2b61851c428738
nickzorzi <nicolas.zorzi@sjsu.edu>
2025-03-09 22:57:08 -0700

    fixed build bug

--- Assets/Scripts/Shop/Silver.cs
+++ Assets/Scripts/Shop/Silver.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Silver : MonoBehaviour
